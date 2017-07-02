6252508adba924453d51f4704512e340dfc40875
sogaani <soga.ani@gmail.com>
2017-07-02 18:20:02 +0900

    swampSyncで非同期処理がせき止められていたようなので、使わないように修正

--- src/amumu_encoder.js
+++ src/amumu_encoder.js
@@ -1,41 +1,29 @@
 "use strict";
 
+const request = require('request');
 const Agenda = require('agenda');
 const { MongoClient } = require('mongodb');
 const child_process = require('child_process');
 const CONFIG_FILE = __dirname + '/../encoder_config.json';
 const config = require(CONFIG_FILE);
-require('request').debug = true;
-const request = require('request');
 
-async function run() {
-    const db = await MongoClient.connect(config.mongodbPath + 'amumu',{
+
+function run() {
+    var dboptions = {
         server: {
             socketOptions: {
                 socketTimeoutMS: 0,
                 connectTimeoutMS: 0
             }
         }
-    });
-    const agenda = new Agenda(config.mongodbPath + 'amumu').mongo(db, 'jobs');
+    };
+    const agenda = new Agenda({db: { address: config.mongodbPath + 'amumu', collection: 'jobs', options: dboptions}});
 
     child_process.execSync('net use ' + config.recordedPath + ' ' + config.remoteAuthPass + ' /user:' + config.remoteAuthUser);
 
-    async function checkCancelled(job) {
-        const count = await db.collection('jobs').count({ _id: job.attrs._id });
-        return !(count > 0);
-    }
-
-    async function failJob(job,err) {
-        job.fail(err);
-        await new Promise(resolve => job.save(resolve()));
-    }
-
-    agenda.define('amumu_encode', async job =>  {
+    agenda.define('amumu_encode', {concurrency: 1, lockLimit: 1, lockLifetime: 120*60*1000},(job,done) =>  {
         try {
-        if (await checkCancelled(job)) {
-            return;
-        }
+
         var args = [];
         console.log("encode start");
         var file = job.attrs.data.recorded.match(/\/([^\/]+?)\.[^\.]+?$/)[1]
@@ -49,62 +37,60 @@ async function run() {
         args.push('-c:a', 'aac');
         args.push( server + file + '.mp4');
 
-        var ffmpeg = child_process.spawnSync('ffmpeg',args,{ stdio: 'ignore' });
-        if ( ffmpeg.status != 0 ){
-            console.log("encode fail");
-            failJob(job,'fail ffmpeg');
-            return;
-        }
-        console.log("encode end");
-
-        var obj = {
-            recorded:   job.attrs.data.recorded.match(/^(.+)\.[^\.]+?$/)[1] + '.mp4'
-        };
-
-        var delopts = {
-            uri: config.chinachuPath + 'api/recorded/' + job.attrs.data.id +'/file.json',
-            method: 'DELETE',
-            timeout: 30 * 1000,
-            agent: false
-        };
-        var putopts = {
-            uri: config.chinachuPath + 'api/recorded/' + job.attrs.data.id +'.json',
-            method: 'PUT',
-            timeout: 30 * 1000,
-            agent: false,
-            form: {
-                json: JSON.stringify(obj)
+        var ffmpeg = child_process.spawn('ffmpeg',args,{ stdio: 'ignore' });
+        ffmpeg.on('exit', function(code) {
+            if ( code != 0 ){
+                console.log("encode fail");
+                job.fail(code);
+                job.save(() => done());
+                return;
             }
-        };
-        var status = true;
+            var obj = {
+                recorded:   job.attrs.data.recorded.match(/^(.+)\.[^\.]+?$/)[1] + '.mp4'
+            };
 
-        request(delopts,requestCallback);
+            var delopts = {
+                uri: config.chinachuPath + 'api/recorded/' + job.attrs.data.id +'/file.json',
+                method: 'DELETE',
+                followRedirect: true
+            };
+            var putopts = {
+                uri: config.chinachuPath + 'api/recorded/' + job.attrs.data.id +'.json',
+                method: 'PUT',
+                followRedirect: true,
+                form: {
+                    json: JSON.stringify(obj)
+                }
+            };
 
-        request(putopts,requestCallback);
+            request(delopts, (err, response, body) => {
+                if (!err){
+                    request(putopts, (err, response, body) => {
+                        if (!err){
+                            console.log("encode end");
+                            done();
+                        }else{
+                            console.log("put fail " + err);
+                            job.fail(err);
+                            job.save(() => done());
+                        }
+                    });
+                }else{
+                    console.log("del fail " + err);
+                    job.fail(err);
+                    job.save(() => done());
+                }
+            });
+        });
+
         } catch (err) {
-            console.error(`oops!` + err);
+            done();
+            console.error('oops! ' + err);
         }
     });
 
-    await new Promise(resolve => agenda.once('ready', resolve()));
-
-    agenda.start();
+    agenda.once('ready', () => agenda.start());
 }
 
 run();
-
-function requestCallback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-}
-
-function promiseCallback(resolve, reject) {
-    return function(error, res) {
-        if (error) {
-        return reject(error);
-        }
-        resolve(res);
-    };
-}
 
